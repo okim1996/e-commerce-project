7ba8ec765ef7b25b02cccc0dc5ba8f86a3677b44
Oscar Kim <oscarkim32@gmail.com>
2025-01-20 14:43:39 -0800

    successfully had frontend and backend communicate with each other

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:5173", // Frontend Url
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed Headers
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
-  res.send("application is running");
+  res.json({ message: "application is running!" });
 });
 
 const server = app.listen(PORT, () => {
--- frontend/src/components/SimpleFetch.tsx
+++ frontend/src/components/SimpleFetch.tsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
-export default function SimpleFetch() {
-  const [backend, getBackend] = useState("default");
-  async function getBackend() {
-    console.log("you clicked on backend button");
-    try {
-      const response = await fetch("http://localhost:4000");
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
+import { useEffect, useState } from "react";
 
-      const data = await response.json();
-      console.log(`Data fetched from backend ${data}`);
-      return data;
-    } catch (error) {
-      console.log(`Error fetching data: ${error}`);
-      return null;
+export default function SimpleFetch() {
+  const [text, setText] = useState("default");
+
+  useEffect(() => {
+    async function fetchBackend() {
+      try {
+        const response = await fetch("http://localhost:4000");
+        if (!response.ok) {
+          throw new Error(`Error : ${response.statusText}`);
+        }
+        const data = await response.json();
+        console.log(data.message);
+      } catch (error) {
+        console.log(`Error fetching data: ${error}`);
+        return null;
+      }
     }
-  }
+    fetchBackend();
+  }, []);
   return (
     <div>
       <p>this is a simple fetch component</p>
-      {/* <button onClick={getBackend}>backend</button> */}
+      <p>{text}</p>
     </div>
   );
 }
